669d14e8dc5d6b249aa49e6bff5a5837cd1479ca
tsomielvis@gmail.com <tsomielvis@gmail.com>
2024-10-12 22:06:50 +0300

    complete

--- server/seed.py
+++ server/seed.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-from app import app
-from models import db, Restaurant, Pizza, RestaurantPizza
+from app import app, db
+from models import Restaurant, Pizza, RestaurantPizza
 
 with app.app_context():
+    db.create_all()
 
     # This will delete any existing rows
     # so you can run the seed file multiple times without having duplicate entries in your database
